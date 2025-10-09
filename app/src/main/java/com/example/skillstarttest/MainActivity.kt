9827a05203414ea8cb3b5f24cfb11e2ffafc805d
Evgeny <bulon.danilov1999@mail.ru>
2025-10-09 13:07:59 +0200

    Создал BottomNavigation

--- app/src/main/java/com/example/skillstarttest/MainActivity.kt
+++ app/src/main/java/com/example/skillstarttest/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.skillstarttest
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }
+
+    fun onHomeClick(view: View){
+
+    }
+
+    fun onFavoritesClick(view: View){
+    }
+
+    fun onAccountClick(view: View){
+
     }
 }
